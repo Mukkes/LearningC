29de852778dfdb4a8bfaf24f6df2432db3c88538
Marcel Zwart <marcelzwart92@gmail.com>
2025-06-26 22:20:51 +0200

    Add Dynamic allocation program.

---
+++ dynamic_allocation/dynamic_allocation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+int main() {
+    Point *mypoint = NULL;
+
+    mypoint = malloc(sizeof(Point));
+
+    mypoint->x = 10;
+    mypoint->y = 5;
+    printf("mypoint coordinates: %d, %d\n", mypoint->x, mypoint->y);
+
+    free(mypoint);
+    return 0;
+}
