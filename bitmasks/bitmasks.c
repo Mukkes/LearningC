bf1a4f0e0381baf00234834f8ffd55a4057b7fbf
Marcel Zwart <marcelzwart92@gmail.com>
2025-07-11 21:57:18 +0200

    Add Bitmasks program.

---
+++ bitmasks/bitmasks.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdio.h>
+
+const short FLAG_ON = 1 << 0;          // 1  (0x01)
+const short FLAG_MOVEMENT = 1 << 1;    // 2  (0x02)
+const short FLAG_TRANSPARENT = 1 << 2; // 4  (0x04)
+const short FLAG_ALIVE = 1 << 3;
+const short FLAG_BROKEN = 1 << 4;
+const short FLAG_EDIBLE = 1 << 5; // 32 (0x20)
+
+int main() {
+    short attributes = 0;
+
+    attributes |= FLAG_ON;
+    attributes |= FLAG_TRANSPARENT;
+    attributes |= FLAG_BROKEN;
+
+    assert(attributes == (FLAG_ON | FLAG_TRANSPARENT | FLAG_BROKEN));
+
+    attributes &= ~FLAG_TRANSPARENT;
+    attributes &= ~FLAG_BROKEN;
+    attributes |= FLAG_ALIVE;
+
+    assert(attributes == (FLAG_ON | FLAG_ALIVE));
+
+    assert(attributes & FLAG_ALIVE);
+
+    assert(attributes & ~FLAG_BROKEN);
+
+    attributes = FLAG_EDIBLE;
+
+    assert(attributes == FLAG_EDIBLE);
+
+    printf("Done!");
+
+    return 0;
+}
