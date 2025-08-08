fda8e6f49d01635a03b0fd4f20940f74b639c271
Marcel Zwart <marcelzwart92@gmail.com>
2025-08-08 15:49:19 +0200

    Inprogres day02.

---
+++ adventofcode2015/day02/day02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct {
+    int l;
+    int w;
+    int h;
+} Box;
+
+Box ConvertStringToBox(char *line);
+int GetSurfaceArea(Box box);
+int GetRequiredWarppingPaper(Box box);
+
+int main() {
+    FILE *filePointer;
+    filePointer = fopen("input.txt", "r");
+    char line[12];
+    if (filePointer != NULL) {
+        while (fgets(line, sizeof(line), filePointer)) {
+        }
+    }
+    return 0;
+}
+
+int GetSurfaceArea(Box box) {
+    return (2 * box.l * box.w) + (2 * box.w * box.h) + (2 * box.h * box.l);
+}
+
+int GetRequiredWarppingPaper(Box box) {
